25cfa7536a925c3f831dcf250e606b727e0bbba0
rmadar <romain@clratlpo06.in2p3.fr>
2024-11-12 09:20:41 +0100

    Adding 2024 exam

---
+++ exam/today/generate_data.py
@@ -0,0 +1,7 @@
+import bees_simulation as bs
+
+#bs.run_bees_simulation(noise=0, box=0, field=0, attraction=0, run=10)
+#bs.run_bees_simulation(noise=1, box=0, field=0, attraction=0, run=11)
+bs.run_bees_simulation(noise=0, field=0, run=0)
+bs.run_bees_simulation(noise=0, field=1, run=1)
+bs.run_bees_simulation(noise=1, field=0, run=2)
